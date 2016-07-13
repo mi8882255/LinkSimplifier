eda717f87dc4c666ca3589edd5d8ccfe1c9ebaa9
igor <mi8882255@gmail.com>
2016-07-13 14:32:23 +0300

    view change

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
