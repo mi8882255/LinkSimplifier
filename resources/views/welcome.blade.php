c054495af51d3267b75252bc447833cdf34aa371
igor <mi8882255@gmail.com>
2016-07-13 16:06:08 +0300

    view change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,57 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <h1>Api Usage:</h1>
+                <p>You can use JSONP get requests for add/moduify data.</p>
+                <p>Routes list:</p>
+<table>
+    <thead>
+    <tr>
+        <th>Url</th><th>Description</th><th>Request params</th><th>Response structure</th><th></th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+            $tableArr=[
+                    ['/api/bookmark/add','add new bookmark','url','id',''],
+                    ['/api/bookmark/get10','get last 10 bookmarks','-',"0:['id','url'],1:...",''],
+                    ['/api/bookmark/getbyid','get bookmark by id with comments','id',"'id','url','comments':[0:['id','created','updated','text'],1:...]",''],
+                    ['/api/comments/add','add new comment','bm_id,text','id',''],
+                    ['/api/comments/modify','modify comment','id,text','id',''],
+                    ['/api/comments/delete','delete comment','id','true|false','']
+            ];
+
+    foreach ($tableArr as $line) {
+        echo "<tr>";
+        foreach ($line as $el) {
+            echo "<td>".$el."</td>";
+        }
+        echo "</tr>";
+    }
+    ?>
+
+    </tbody>
+</table>
+                <hr>
+                <p>JQuery usage example:</p>
+                <code>
+                    $.ajax({
+                    url: "/api/comments/add",
+                    type: "POST",
+                    jsonp: "callback",
+
+                    dataType: "jsonp",
+
+                    data: {
+                    bm_id: "1",
+                    text:'HelloAll12'
+                    },
+
+                    success: function( response ) {
+                    console.log( response ); // server response
+                    }
+                    });
+                </code>
             </div>
         </div>
     </body>
