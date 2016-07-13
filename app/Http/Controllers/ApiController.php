fbc8df2be6b8fab368a2d6cb1c1ca15b329817b6
igor <mi8882255@gmail.com>
2016-07-13 09:57:11 +0300

    create api structure

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ApiController extends Controller
+{
+    public function get10() {
+        
+    }
+    public function getByIdwComments() {
+
+    }
+    public function addNew() {
+
+    }
+    public function addComment() {
+
+    }
+    public function modifyComment() {
+
+    }
+    public function deleteComment() {
+
+    }
+}
